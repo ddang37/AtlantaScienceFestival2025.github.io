21116f62088a67238bde8c9e41a5d2c6036d331e
DiemmyDang <162947716+ddang37@users.noreply.github.com>
2025-02-28 11:50:12 -0500

    add clues and heading

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
-function checkPassword() {
-    const passwordInput = document.getElementById("password").value;
-    const correctPassword = "123"; // Replace with your desired password
 
-    if (passwordInput === correctPassword) {
+
+const cluePasswords = ["00", "11", "22", "33", "44", "55", "66"];
+
+
+
+
+function checkPassword() {
+    const clueInput = document.getElementById("clueNumber");
+    const passwordInput = document.getElementById("password");
+    const correctPassword = cluePasswords[clueInput.value];
+
+    if (passwordInput.value === correctPassword) {
         document.getElementById("message").textContent = "Access granted!";
         document.getElementById("error-message").textContent = "";
         window.location.href = "results.html";
     } else {
         document.getElementById("message").textContent = "";
-        document.getElementById("error-message").textContent = "Incorrect password";
+        //document.getElementById("error-message").textContent = "Incorrect password";
+        const errorMessage = document.getElementById("error-message");
+        showTextBySeconds(errorMessage, "Incorrect password", 1);
+        passwordInput.value = "";
     }
 }
+
+function showTextBySeconds(textElement, text, second) {
+    textElement.textContent = text;
+    setTimeout(() => {
+      textElement.textContent = '';
+    }, second * 1000);
+  }
