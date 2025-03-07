7670c64a4b66277f59a5ffd6c27047ddbf4f2806
DiemmyDang <162947716+ddang37@users.noreply.github.com>
2025-03-07 13:09:39 -0500

    make the directing work and change context

--- index.js
+++ index.js
@@ -1,30 +1,10 @@
-
-
-const cluePasswords = ["password", "phishing", "ransomware", "firewall", "mitma", "database"];
-
-
-function checkPassword() {
-    const clueInput = document.getElementById("clueNumber");
-    const passwordInput = document.getElementById("password");
-    const correctPassword = cluePasswords[clueInput.value - 1];
-    
-
-    if (passwordInput.value.toLowerCase() === correctPassword.toLowerCase()) {
-        document.getElementById("message").textContent = "Access granted!";
-        document.getElementById("error-message").textContent = "";
-        window.location.href = "clues.html";
-    } else {
-        document.getElementById("message").textContent = "";
-        //document.getElementById("error-message").textContent = "Incorrect password";
-        const errorMessage = document.getElementById("error-message");
-        showTextBySeconds(errorMessage, "Incorrect password", 1);
-        passwordInput.value = "";
-        clueInput.value = "";
-    }
-}
 function showTextBySeconds(textElement, text, second) {
     textElement.textContent = text;
     setTimeout(() => {
       textElement.textContent = '';
     }, second * 1000);
-  }
+}
+
+function goHome() {
+    window.location.href = "home.html";
+}
