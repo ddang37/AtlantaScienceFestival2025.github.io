74746856488a49759652a6fa376bdc6a51c55fca
DiemmyDang <162947716+ddang37@users.noreply.github.com>
2025-03-08 00:55:45 -0500

    add clue 0

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const cluePasswords = ["atlanta", "password", "phishing", "ransomware", "firewall", "mitma", "database"];
+
 function showTextBySeconds(textElement, text, second) {
     textElement.textContent = text;
     setTimeout(() => {
@@ -7,4 +9,31 @@ function showTextBySeconds(textElement, text, second) {
 
 function goHome() {
     window.location.href = "home.html";
+}
+
+function checkPassword(aClue, aPassword) {
+    let isCorrect = false;
+    const correctPassword = cluePasswords[aClue % cluePasswords.length];
+
+    const message = document.getElementById("message");
+    const errorMessage = document.getElementById("error-message");
+
+    // Check if clue is valid
+    if (isNaN(aClue) || aClue < 0 || aClue >= cluePasswords.length) {
+        message.textContent = "";
+        showTextBySeconds(errorMessage, "Invalid clue number", 1);
+    } 
+    // Check if the password is correct
+    else if (aPassword.toLowerCase() === correctPassword.toLowerCase()) {
+        showTextBySeconds(message, "Access granted", 3);
+        isCorrect = true;
+    } 
+    // If the password is incorrect
+    else {
+        showTextBySeconds(errorMessage, "Incorrect password", 1);
+    }
+
+    aPassword = ""; // Clear the password input
+    aClue = ""; // Clear the clue input
+    return isCorrect;
 }
